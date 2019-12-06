cd362a70aaf7794891d054077f40b3a5df9cdb6a
jose restrepo <arturo.restrepo@gmail.com>
2019-12-06 12:46:58 -0500

    draft setup classes

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -18,4 +18,24 @@
 #
 # Put a comment noting which class is the base class
 
-#my first commit
+# YOUR CODE HERE
+
+class Vehicle: #base class
+
+    def __init__(self, vehicle, flight_vehicle, ground_vehicle, car, motorcycle, starship, airplane):
+        self.vehicle = Vehicle
+        self.flight_vehicle = FlightVehicle
+        self.ground_vehicle = GroundVehicle
+        self.car = Car
+        self.motorcycle = Motorcycle
+        self.starship = Starship
+        self.airplane = Airplane
+
+class Craftever(Vehicle):
+
+    def __init__(self, body, vehicle, flight_vehicle, ground_vehicle, car, motorcycle, starship, airplane):
+        super().__init__(vehicle, flight_vehicle, ground_vehicle, car, motorcycle, starship, airplane)
+        self.name = body
+
+    def __str__(self):
+        return 'Craftever { body = '+self.body+', vehicle = '+str(self.vehicle)+', flight_vehicle = '+str(self.flight_vehicle)+', ground_vehicle = '+str(self.ground_vehicle)+', car = '+str(self.car)+', motorcycle = '+str(self.motorcycle)+', starship = '+str(self.startship)+', airplane = '+str(self.airplane)+'}'
