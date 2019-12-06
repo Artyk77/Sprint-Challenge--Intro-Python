59b0c557758bdd10be7460b83551bef0c02e5d4c
jose restrepo <arturo.restrepo@gmail.com>
2019-12-06 12:58:49 -0500

    revamped and tested

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -18,24 +18,25 @@
 #
 # Put a comment noting which class is the base class
 
-# YOUR CODE HERE
+# Base class
 
-class Vehicle: #base class
+class Vehicle:
+    pass
 
-    def __init__(self, vehicle, flight_vehicle, ground_vehicle, car, motorcycle, starship, airplane):
-        self.vehicle = Vehicle
-        self.flight_vehicle = FlightVehicle
-        self.ground_vehicle = GroundVehicle
-        self.car = Car
-        self.motorcycle = Motorcycle
-        self.starship = Starship
-        self.airplane = Airplane
+class FlightVehicle(Vehicle):
+    pass
 
-class Craftever(Vehicle):
+class Starship(FlightVehicle):
+    pass
 
-    def __init__(self, body, vehicle, flight_vehicle, ground_vehicle, car, motorcycle, starship, airplane):
-        super().__init__(vehicle, flight_vehicle, ground_vehicle, car, motorcycle, starship, airplane)
-        self.name = body
+class Airplane(FlightVehicle):
+    pass
 
-    def __str__(self):
-        return 'Craftever { body = '+self.body+', vehicle = '+str(self.vehicle)+', flight_vehicle = '+str(self.flight_vehicle)+', ground_vehicle = '+str(self.ground_vehicle)+', car = '+str(self.car)+', motorcycle = '+str(self.motorcycle)+', starship = '+str(self.startship)+', airplane = '+str(self.airplane)+'}'
+class GroundVehicle(Vehicle):
+    pass
+
+class Car(GroundVehicle):
+    pass
+
+class Motorcycle(GroundVehicle):
+    pass
